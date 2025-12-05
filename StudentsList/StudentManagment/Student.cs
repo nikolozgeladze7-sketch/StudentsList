6d134a1c35a4fa1071d85234984c6973d0db04a2
NikolozGeladze <nikolozgeladze7@gmail.com>
2025-12-05 12:56:35 +0400

    Add files via upload

---
+++ StudentsList/StudentManagment/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace StudentsList.StudentManagment
+{
+    /// <summary>
+    /// Represents a student with a number and grade, inherited from the <see cref="Person"/> class.
+    /// </summary>
+    public class Student : Person
+    {
+        public int RollNumber { get; set; }
+        public char Grade { get; set; }
+
+        public Student(string name, int rollNumber, char grade)
+            : base(name)
+        {
+            RollNumber = rollNumber;
+            Grade = grade;
+        }
+
+        /// <summary>
+        /// Overrides the DisplayInfo method to include roll number and grade.
+        /// </summary>
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Roll Number: {RollNumber}, Grade: {Grade}");
+        }
+    }
+}
